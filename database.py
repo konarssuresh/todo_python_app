21fcf1c51e99e25fc3cd55383bd023e7197c4548
Suresh Konar <konarssuresh@gmail.com>
2026-06-30 14:52:18 +0530

    added database url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL="postgresql://postgres:test1234!@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL="postgresql://root:RZj0cnZNYg3QbvhaRyceEna1H3btB9rQ@dpg-d91ohtpo3t8c73ee7tn0-a.singapore-postgres.render.com/todoapplicationdatabase_iu5e"
 
 # SQLALCHEMY_DATABASE_URL="mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase"
 
